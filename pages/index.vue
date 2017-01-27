45b3e5dad027f89c953a2508b6d4e53ad9c91101
johnlindquist <johnlindquist>
2017-01-27 16:35:52 -0700

    vuex-mutations

---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    {{counter}}
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
+  </div>
+</template>
+<script>
+import {mapState, mapMutations} from 'vuex'
+
+export default {
+  computed:{
+    ...mapState({
+      counter: state => state.counter      
+    })
+  },
+
+  methods:{
+    ...mapMutations([
+      'increment',
+      'decrement'
+    ])
+  }
+}
+</script>
---
+++ store/index.js
@@ -0,0 +1,12 @@
+export const state = {
+    counter: 0
+}
+
+export const mutations = {
+    increment(state) {
+        state.counter++
+    },
+    decrement(state) {
+        state.counter--
+    }
+}
