cf51b3407193620ff75f39fe3ec4bd3424c847c8
johnlindquist <johnlindquist>
2017-01-27 17:10:57 -0700

    add-todos

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,13 @@
 <template>
-  <div class="pa4">
-    {{counter}}
-    <button @click="increment">+</button>
-    <button @click="decrement">-</button>
+  <div>
+    <article class="pa3 pa5-ns">
+      <h1 class="f4 bold center mw6">Todos</h1>
+      <ul class="list pl0 ml0 center mw6 ba b--light-silver br2">
+        <li v-for="todo of todos" class="ph3 pv3 bb b--light-silver">
+          {{todo.task}}
+        </li>
+      </ul>
+    </article>
   </div>
 </template>
 <script>
@@ -11,15 +16,12 @@ import {mapState, mapMutations} from 'vuex'
 export default {
   computed:{
     ...mapState({
-      counter: state => state.counter      
+      todos: state => state.todos      
     })
   },
 
   methods:{
-    ...mapMutations([
-      'increment',
-      'decrement'
-    ])
+
   }
 }
 </script>
--- store/index.js
+++ store/index.js
@@ -1,12 +1,11 @@
 export const state = {
-    counter: 0
+    todos: [
+        { task: "eat" },
+        { task: "sleep" },
+        { task: "code" },
+    ]
 }
 
 export const mutations = {
-    increment(state) {
-        state.counter++
-    },
-    decrement(state) {
-        state.counter--
-    }
+
 }
