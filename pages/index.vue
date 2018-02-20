5c2e57055454157f3b2bdc9ccc816ec669b51247
John Lindquist <johnlindquist@computer.local>
2018-02-20 15:41:04 -0700

    mutations

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,18 @@
 <template>
   <section class="p-4">
     <h1>{{count}}</h1>
+    <button @click="increment" class="bg-blue text-white py-2 px-4 rounded">+</button>
   </section>
 </template>
 <script>
-import { mapState } from "vuex"
+import { mapState, mapMutations } from "vuex"
 
 export default {
   computed: {
     ...mapState(["count"])
+  },
+  methods: {
+    ...mapMutations(["increment"])
   }
 }
 </script>
--- store/index.js
+++ store/index.js
@@ -1,3 +1,9 @@
 export const state = () => ({
   count: 0
 })
+
+export const mutations = {
+  increment(state) {
+    state.count++
+  }
+}
