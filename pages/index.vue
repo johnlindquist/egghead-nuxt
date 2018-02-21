5cf230b883ea8118ce30e41ed9dfbb2542cad690
John Lindquist <johnlindquist@computer.local>
2018-02-21 13:03:19 -0700

    actions

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,18 @@
 <template>
   <section class="p-4">
     <h1>{{count}}</h1>
-    <button @click="increment" class="bg-blue text-white py-2 px-4 rounded">+</button>
+    <button @click="incrementAction" class="bg-blue text-white py-2 px-4 rounded">+</button>
   </section>
 </template>
 <script>
-import { mapState, mapMutations } from "vuex"
+import { mapState, mapActions } from "vuex"
 
 export default {
   computed: {
     ...mapState(["count"])
   },
   methods: {
-    ...mapMutations(["increment"])
+    ...mapActions(["incrementAction"])
   }
 }
 </script>
--- store/index.js
+++ store/index.js
@@ -5,5 +5,18 @@ export const state = () => ({
 export const mutations = {
   increment(state) {
     state.count++
+  },
+  reset(state) {
+    state.count = 0
+  }
+}
+
+export const actions = {
+  incrementAction({ state, commit }) {
+    if (state.count === 3) {
+      commit("reset")
+    } else {
+      commit("increment")
+    }
   }
 }
